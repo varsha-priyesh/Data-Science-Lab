c1ac122c8d0f24aa97cf6609a66acda27d2b5aa1
haibzzhbz <74949778+haibzzhbz@users.noreply.github.com>
2021-11-17 13:43:04 +0530

    Add files via upload

---
+++ 20-Hiba Nafeesath/2nd.py
@@ -0,0 +1,2 @@
+lists=['hiba','haneen','shafeeq','sinan','hiba']
+print(list(set(lists)))
---
+++ 20-Hiba Nafeesath/3rd.py
@@ -0,0 +1,7 @@
+def convert(tup,di):
+    di=dict(tup)
+    return di
+tuples=[("Hiba",95),("shfq",99),("basila",80),("nadeer",85)]
+print(tuples)
+dictionary={}
+print(convert(tuples,dictionary))
