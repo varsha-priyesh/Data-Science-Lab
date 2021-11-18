a080173fa1c944bdbf8b68d2b1fc37aea3e352d6
SoorajM2 <75290483+SoorajM2@users.noreply.github.com>
2021-11-18 13:27:46 +0530

    Add files via upload

---
+++ 53-SOORAJ.M/program1.py
@@ -0,0 +1,6 @@
+Python 3.8.8 (tags/v3.8.8:024d805, Feb 19 2021, 13:18:16) [MSC v.1928 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> list=['5','3','54','67']
+>>> print(max(list))
+67
+>>> 
---
+++ 53-SOORAJ.M/program3.py
@@ -0,0 +1,3 @@
+tuple=((1,'ant'),(2,'zebra'))
+print(dict((x,y) for y,x in tuple))
+
