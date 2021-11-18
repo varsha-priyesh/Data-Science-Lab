79d96d3467af3f36ac816c3031cbac611f3d72cc
Safeelanasrin <74948799+Safeelanasrin@users.noreply.github.com>
2021-11-18 13:43:44 +0530

    Add files via upload

---
+++ 42-Safeela Nasrin ck/Largest number.py
@@ -0,0 +1,3 @@
+list1 = [10,2,56,77,90]
+list1.sort()
+print("largrest number of list is:",list1[-1])
---
+++ 42-Safeela Nasrin ck/Program5.py
@@ -0,0 +1,8 @@
+def common_data(list1,list2):
+    for x in list2:
+        if x in list1:
+            result = True
+     return false
+print(common_data([1,2,3,4,5],[5,6,7,8,9]))
+print(commom_data([1,2,3,4,5],[6,7,8,9]))
+      
