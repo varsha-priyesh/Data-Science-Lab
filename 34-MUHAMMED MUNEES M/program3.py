d2c9cdb9d2bd5ab6d241fcd2fa08493b6c18657d
M Munees <67566629+muneesmmm@users.noreply.github.com>
2021-11-18 13:43:22 +0530

    7 programmes done

---
+++ 34-MUHAMMED MUNEES M/program3.py
@@ -0,0 +1,2 @@
+tuple1=((1,'apple'),(2,'grapes'))
+print(dict((x,y) for y,x in tuple1))
---
+++ 34-MUHAMMED MUNEES M/program4.py
@@ -0,0 +1,4 @@
+newDict1={1:'apple', 2:'grapes', 3:'mango'}
+newDict2={4:'pomegranate', 5:'strawberry'}
+newDict1.update(newDict2)
+print(newDict1)
