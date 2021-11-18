c8c813af09052e7175dd24d75f50540768243de2
SAYUJ A P <75025196+SayujAP10@users.noreply.github.com>
2021-11-18 13:42:31 +0530

    Add files via upload

---
+++ 47-SAYUJ A P/program3.py
@@ -0,0 +1,2 @@
+tuple = ((2,"pineapple"),(3,"orange"))
+print(dict((y,x) for x,y in tuple))
---
+++ 47-SAYUJ A P/program7.py
@@ -0,0 +1,4 @@
+from datetime import date,timedelta
+dt=date.today() - timedelta(5)
+print('current date : ',date.today())
+print('5 days before current date :',dt)
