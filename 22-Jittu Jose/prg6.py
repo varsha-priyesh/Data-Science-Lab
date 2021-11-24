adf118e1a32f8a8fcc1c6951f5da23e2c97d8c94
jittujose <74975969+jittujose@users.noreply.github.com>
2021-11-24 14:14:47 +0530

    Add files via upload

---
+++ 22-Jittu Jose/prg6.py
@@ -0,0 +1,12 @@
+from datetime import date,timedelta
+x=date(1997,4,9)
+y=date(1997,5,8)
+print ("date1 is: ",x)
+print ("date2 is: ",y)
+print ("The earliest date is ")
+if x<y:
+    print (x)
+else:
+    print (y)
+
+    
---
+++ 22-Jittu Jose/prg7.py
@@ -0,0 +1,4 @@
+from datetime import date,timedelta
+a=date.today()-timedelta(5)
+print ("Current date :",date.today())
+print ("5 days before current date :",a)
