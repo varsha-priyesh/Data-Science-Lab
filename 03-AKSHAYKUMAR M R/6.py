86b8e7d507012d093ce7d4e026d73de40f31da96
AKSHAYKUMARMR <74973377+AKSHAYKUMARMR@users.noreply.github.com>
2021-11-24 14:51:42 +0530

    Add files via upload

---
+++ 03-AKSHAYKUMAR M R/6.py
@@ -0,0 +1,13 @@
+Python 3.8.8 (tags/v3.8.8:024d805, Feb 19 2021, 13:18:16) [MSC v.1928 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+== RESTART: C:/Users/admin/AppData/Local/Programs/Python/Python38/akshay/6.py ==
+First Date 2020-04-08
+Second Date 2021-04-08
+2020-04-08
+>>> 
+== RESTART: C:/Users/admin/AppData/Local/Programs/Python/Python38/akshay/6.py ==
+First Date 2021-04-08
+Second Date 2022-04-08
+2021-04-08
+>>> 
