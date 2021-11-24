4c89602f9a3ec13ce8dab4e169c5612fa72233cd
lijas008 <74948897+lijas008@users.noreply.github.com>
2021-11-24 14:40:23 +0530

    program6 updated

---
+++ 28-Mohammed Lijas C/program6.1.py
@@ -0,0 +1,9 @@
+from datetime import date
+fdate = date(2020, 4, 8)
+sdate = date(2021, 4, 8)
+print("First Date",fdate)
+print("Second Date",sdate)
+if fdate<sdate:
+    print(fdate)
+else:
+    print(sdate)
