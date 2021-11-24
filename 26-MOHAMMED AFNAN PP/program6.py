b8953d9ca4f69fa436f51a71b3c5425d2b2aa1ba
mohammedAfnanpp <74949184+mohammedAfnanpp@users.noreply.github.com>
2021-11-24 15:10:20 +0530

    Add files via upload

---
+++ 26-MOHAMMED AFNAN PP/program6.py
@@ -0,0 +1,9 @@
+from datetime import date
+fdate = date(2019, 5, 7)
+sdate = date(2021, 5, 7)
+print("First Date",fdate)
+print("Second Date",sdate)
+if fdate<sdate:
+    print(fdate)
+else:
+    print(sdate)
